75f32349f47c1b9cf5218e9e52a14a77b2653b11
Kevin Randrianantenaina <kevin22fr@gmail.com>
2024-09-08 14:37:09 -0400

    gitconfig

--- install.sh
+++ install.sh
@@ -10,10 +10,4 @@ sudo apt-get update
 sudo apt-get install -y git vim gcc gdb python3 python3-pip python3-venv
 
 # Copier les dotfiles
-cp ~/source_dotfiles/.bashrc ~/
-cp ~/source_dotfiles/.vimrc ~/
-mkdir -p ~/.conf
-cp ~/source_dotfiles/* ~/.conf/
-
-nvim ~/.bashrc
-
+cp ~/.dotfiles/* ~/
